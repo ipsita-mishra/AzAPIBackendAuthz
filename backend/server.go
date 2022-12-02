3e600a9555fbe390a820fef3f9e73bed92420f75
Ipsita Mishra <ipsitamishra16893@gmail.com>
2022-12-02 15:00:50 +0100

    processing header

--- backend/server.go
+++ backend/server.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/base64"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -19,7 +21,23 @@ func main() {
 	r := gin.New()
 
 	r.GET("/api/userinfo", func(c *gin.Context) {
-		c.String(http.StatusOK, "Sign In Successful. Here are your details : %s", c.Request.Header["X-Ms-Client-Principal"])
+		val, ok := c.Request.Header["X-Ms-Client-Principal"]
+		decodedUserInfo, _ := base64.StdEncoding.DecodeString((strings.Join(val, "")))
+		if ok {
+			c.String(http.StatusOK, "Here are your details : %s", decodedUserInfo)
+		} else {
+			c.String(http.StatusOK, "Please login to view the information.")
+		}
+	})
+
+	r.GET("/api/privi", func(c *gin.Context) {
+		val, ok := c.Request.Header["X-Ms-Client-Principal"]
+		decodedUserInfo, _ := base64.StdEncoding.DecodeString((strings.Join(val, "")))
+		if ok {
+			c.String(http.StatusOK, "Your RBAC role is : %s .Here are details you are allowed to view : %s", decodedUserInfo)
+		} else {
+			c.String(http.StatusOK, "Please login to view the information.")
+		}
 	})
 
 	log.Printf("Listening on port %s", port)
